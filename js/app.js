cf576a8bb9725002b381a1146f21b17ec8a6575d
marina-fares <marina.fares11@gmail.com>
2020-07-22 13:07:46 +0200

    add changes

--- js/app.js
+++ js/app.js
@@ -54,4 +54,90 @@
 
 // Set sections as active
 
-console.log("Heloooooo");
+// start time
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('timer').innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
+// Simple calculator 
+function pushBtn(obj){
+    var inputtest = document.getElementById('inputlabel');
+    var pushed = obj.innerHTML;
+    if (pushed == '='){
+        inputtest.innerHTML = eval(inputtest.innerHTML);
+    }
+    else if (pushed == 'AC'){
+        inputtest.innerHTML = '0';
+    }
+    else{
+        if(inputtest.innerHTML == '0'){
+            inputtest.innerHTML = pushed;
+        }else{
+            inputtest.innerHTML +=pushed;
+        }
+    }
+}
+
+
+// counter 
+
+let counter = 0;
+
+function plus(obj){
+    
+    counter++;
+    var innercount = document.getElementById('counter');
+    innercount.innerHTML = counter;
+ //   console.log(counter);
+//    console.log(innercount.innerHTML);
+}
+function minus(obj){
+    
+    counter--;
+    var innercount = document.getElementById('counter');
+    innercount.innerHTML = counter;
+ //   console.log(counter);
+//    console.log(innercount.innerHTML);
+}
+function Reset(obj){
+    var innercount = document.getElementById('counter');
+    innercount.innerHTML = 0;
+    counter = 0;
+    
+}
+
+// scroll button function 
+window.onload = ()=> {
+    startTime();
+    var mybutton = document.getElementById("myBtn");
+    window.onscroll = () => {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+};
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+
+
+
+
+ 
